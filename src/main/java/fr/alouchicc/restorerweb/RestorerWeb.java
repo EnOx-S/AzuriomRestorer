14d5eea38ae522b1f654e09d827a24aecfea0abb
EnOx_S <sundisnaily7@gmail.com>
2024-01-17 14:17:47 +0100

    Switch to last version of Skin Restorer and switch skin change system

--- src/main/java/fr/alouchicc/restorerweb/RestorerWeb.java
+++ src/main/java/fr/alouchicc/restorerweb/RestorerWeb.java
@@ -1,7 +1,12 @@
 package fr.alouchicc.restorerweb;
 
-import net.skinsrestorer.api.SkinsRestorerAPI;
-import net.skinsrestorer.api.exception.SkinRequestException;
+import net.skinsrestorer.api.SkinsRestorer;
+import net.skinsrestorer.api.exception.DataRequestException;
+import net.skinsrestorer.api.exception.MineSkinException;
+import net.skinsrestorer.api.property.InputDataResult;
+import net.skinsrestorer.api.storage.PlayerStorage;
+import net.skinsrestorer.api.storage.SkinStorage;
+
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
@@ -12,15 +17,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
 
 public final class RestorerWeb extends JavaPlugin implements Listener {
 
-    private SkinsRestorerAPI skinsRestorerAPI;
+    private SkinsRestorer skinsRestorerAPI;
     public File jobFile;
 
     @Override
     public void onEnable() {
-        System.out.println("§7Plugin RestorerWeb by §3AlouchiCC &7fix by §3EnOx_S");
+        System.out.println("§7Plugin RestorerWeb by §3AlouchiCC §7fix by §3EnOx_S");
         getServer().getPluginManager().registerEvents(this, this);
 
         createFile("config");
@@ -50,12 +56,20 @@ public final class RestorerWeb extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void Skins(PlayerJoinEvent event) throws SkinRequestException {
-        Player player = event.getPlayer();
-        skinsRestorerAPI.setSkin(player.getName(), getConfig().getString("url") + player.getName());
+    public void Skins(PlayerJoinEvent event) {
+        try {
+            Player player = event.getPlayer();
+            SkinStorage skinStorage = skinsRestorerAPI.getSkinStorage();
+            Optional<InputDataResult> result = skinStorage.findOrCreateSkinData(getConfig().getString("url") + player.getName());
+            PlayerStorage playerStorage = skinsRestorerAPI.getPlayerStorage();
+            playerStorage.setSkinIdOfPlayer(player.getUniqueId(), result.get().getIdentifier());
+            skinsRestorerAPI.getSkinApplier(Player.class).applySkin(player);
+        } catch (DataRequestException | MineSkinException e) {
+            e.printStackTrace();
+        }
     }
     @Override
     public void onDisable() {
-        System.out.println("§7Plugin RestorerWeb by §3AlouchiCC &7fix by §3EnOx_S");
+        System.out.println("§7Plugin RestorerWeb by §3AlouchiCC §7fix by §3EnOx_S");
     }
 }
